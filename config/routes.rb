daa956527b1aa8d4d57e943f44524a798fbd29b0
metrio <dlima.nyit@gmail.com>
2020-11-13 13:36:54 -0500

    Double Checking

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :businesses
   resources :reviews
   resources :users
-  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
 
   delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
